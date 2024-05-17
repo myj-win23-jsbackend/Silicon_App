c9dccabe2c0ed9793597805900972405940087ff
myj97 <myj97@RAZER_8>
2024-05-17 16:47:39 +0200

    Add project files.

---
+++ Silicon_BlazorApp/Silicon_BlazorApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Silicon_BlazorApp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
