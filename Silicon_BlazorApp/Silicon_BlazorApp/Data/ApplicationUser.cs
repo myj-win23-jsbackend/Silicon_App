2d2781c8325517d6a8f0d8a2b09e7f78dfd10a1f
myj97 <my.j97@protonmail.com>
2024-07-18 22:02:39 +0200

    applicationuser 1:50:05

--- Silicon_BlazorApp/Silicon_BlazorApp/Data/ApplicationUser.cs
+++ Silicon_BlazorApp/Silicon_BlazorApp/Data/ApplicationUser.cs
@@ -1,10 +1,25 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
-namespace Silicon_BlazorApp.Data
+namespace Silicon_BlazorApp.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
-    {
-    }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string? Biography { get; set; }
+    public string? ProfileImage { get; set; } = "avatar.jpg";
+    public int? AddressId { get; set; }
+    public AddressEntity? Address { get; set; }
+}
 
+public class AddressEntity
+{
+    [Key]
+    public int Id { get; set; }
+    public string AddressLine_1 { get; set; } = null!;
+    public string? AddressLine_2 { get; set; }
+    public string PostalCode { get; set; } = null!;
+    public string City { get; set; } = null!;
 }
