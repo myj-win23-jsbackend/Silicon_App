4b8d84d650017f007d5fd74ebf3c511834b3017c
myj97 <my.j97@protonmail.com>
2024-07-18 18:48:57 +0200

    register terms and conditions 1:46:28

---
+++ Silicon_BlazorApp/Silicon_BlazorApp/Filters/CheckboxRequired.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Silicon_BlazorApp.Filters;
+
+public class CheckboxRequired : ValidationAttribute
+{
+    public override bool IsValid(object? value) => value is bool b && b;
+}
